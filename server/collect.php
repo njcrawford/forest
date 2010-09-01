c2d92df0287200028315f09846e71260fb5c1e07
Nathan Crawford <njcrawford@gmail.com>
2010-09-01 00:02:34 +0000

    Working version of test code

--- server/collect.php
+++ server/collect.php
@@ -8,29 +8,32 @@ $db_user = "forest_user";
 $db_password = "forest_pass";
 $db_name = "forest";
 
-mysql_connect($db_server, $db_user, $db_password);
-mysql_select_db($db_name);
+#mysql_connect($db_server, $db_user, $db_password);
+#mysql_select_db($db_name);
 
 //look through post data and see if there's anything to save
 if(!empty($_POST[$post_data_name]))
 {
 	$data_ok = true;
+	$system_id = $_POST['system_id'];
+	echo "system_id: " . $system_id . "<br />";
 	$packages = explode(",", $_POST[$post_data_name]);
 	foreach($packages as $this_package)
 	{
 		//if there is, build an SQL query and run it
-		$result = mysql_query("insert into " . $updates_table . " values
-			(
-				null,
-				'system_id',
-				'" . $this_package . "',
-				NOW()
-			)");
-		if(!$result)
-		{
-			$data_ok = false;
-			break;
-		}
+		echo $this_package . "<br />";
+#		$result = mysql_query("insert into " . $updates_table . " values
+#			(
+#				null,
+#				'" . $system_id . "',
+#				'" . $this_package . "',
+#				NOW()
+#			)");
+#		if(!$result)
+#		{
+#			$data_ok = false;
+#			break;
+#		}
 	}
 	//send back a message indicating data received (or not)
 	if($data_ok)
