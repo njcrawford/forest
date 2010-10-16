c8e8f88887e671a8a97bea7021041f26ade87ba8
Nathan Crawford <njcrawford@gmail.com>
2010-10-16 04:48:21 +0000

    Improved reboot required clarity

--- server/www/index.php
+++ server/www/index.php
@@ -25,7 +25,19 @@ while($row)
 	$result2 = mysql_query("select count(package_name) as packages from updates where system_id = '" . $row['id'] . "'");
 	$row2 = mysql_fetch_assoc($result2);
 	
-	echo "<tr><td>" . $row['name'] . "</td><td>" . $row2['packages'] . "</td><td>" . $row['reboot_required'] . "</td><td>" . $row['last_checkin'] . "</td></tr>";
+	if($row['reboot_required'] == null)
+	{
+		$nice_reboot = "Unknown";
+	}
+	elseif($row['reboot_required'] == 1)
+	{
+		$nice_reboot = "Yes";
+	}
+	else
+	{
+		$nice_reboot = "No";
+	}
+	echo "<tr><td>" . $row['name'] . "</td><td>" . $row2['packages'] . "</td><td>" . $nice_reboot . "</td><td>" . $row['last_checkin'] . "</td></tr>";
 
 	$row = mysql_fetch_assoc($result);
 }
