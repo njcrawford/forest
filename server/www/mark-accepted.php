d7778b3f5c1814789c5a2511ea44ae5ed542b091
Nathan Crawford <njcrawford@gmail.com>
2010-10-24 03:28:19 +0000

    Added buttons to mark packages as selected

--- server/www/mark-accepted.php
+++ server/www/mark-accepted.php
@@ -6,26 +6,52 @@ function redirect_back()
 	header("Location: " . $_SERVER['HTTP_REFERER'], 307);
 }
 
+// make sure the basic required POST stuff is here
+if(!isset($_POST['system_id']) && !isset($_POST['package']))
+{
+	die("No package or system specified");
+}
+if(!isset($_POST['accepted']))
+{
+	die("No accepted value");
+}
+elseif($_POST['accepted'] != "true" && $_POST['accepted'] != "false")
+{
+	die("Invalid accepted value");
+}
+$nice_accepted = ($_POST['accepted'] == "true") ? 1 : 0;
+$query = "update updates set accepted = '" . $nice_accepted . "' where ";
 if(isset($_POST['system_id']) && isset($_POST['package']))
 {
 	// specific system/package combo
-	mysql_query("update updates set accepted = true where system_id = '" . $_POST['system_id'] . "' and package_name = '" . $_POST['package']. "'");
-	redirect_back();
+	$query .= "system_id = '" . $_POST['system_id'] . "' and package_name = '" . $_POST['package']. "'";
 }
-elseif($isset($_POST['system_id']))
+elseif(isset($_POST['system_id']))
 {
 	// all updates for a system
-	mysql_query("update updates set accepted = true where system_id = '" . $_POST['system_id'] . "'");
-	redirect_back();
+	$query .= "system_id = '" . $_POST['system_id'] . "'";
 }
-elseif($isset($_POST['package']))
+elseif(isset($_POST['package']))
 {
 	// all systems for a specific package
-	mysql_query("update updates set accepted = true where package_name = '" . $_POST['package_name'] . "'");
+	$query .= "package_name = '" . $_POST['package'] . "'";
+}
+
+$db_server = "localhost";
+$db_user = "forest_user";
+$db_password = "forest_pass";
+$db_name = "forest";
+
+mysql_connect($db_server, $db_user, $db_password);
+mysql_select_db($db_name);
+
+$result = mysql_query($query);
+if($result)
+{
 	redirect_back();
 }
 else
 {
-	echo "No package or system specified";
+	echo "Error: " . mysql_error();
 }
 ?>
