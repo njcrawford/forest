e57da5b6e0b994167f7bac8d800652b2eaae12a1
Nathan Crawford <njcrawford@gmail.com>
2010-10-26 20:51:37 +0000

    Only return accepted updates, not all of them

--- server/www/getaccepted.php
+++ server/www/getaccepted.php
@@ -16,7 +16,7 @@ if(!empty($_GET['system']))
 	$system_result = mysql_query("select * from systems where name = '" . $_GET['system'] . "'");
 	$system_row = mysql_fetch_assoc($system_result);
 
-	$updates_result = mysql_query("select * from updates where system_id = '" . $system_row['id'] . "'");
+	$updates_result = mysql_query("select * from updates where system_id = '" . $system_row['id'] . "' and accepted = '1'");
 	$updates_row = mysql_fetch_assoc($updates_result);
 
 	while($updates_row)
