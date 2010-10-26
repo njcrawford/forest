204d58da50e8cfa16bfd19eb1774e036ce8c5d17
Nathan Crawford <njcrawford@gmail.com>
2010-10-26 17:36:50 +0000

    Updated install script with new names

--- client/install.sh
+++ client/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp forest-collect /usr/sbin/forest-collect
-cp forest-collect-cron /etc/cron.daily/forest-collect
+cp forest-client /usr/sbin/forest-client
+cp forest-client-cron /etc/cron.daily/forest-client
 cp forest-client.conf.dist /etc/forest-client.conf
