134e1f530db7cd6ed685d9afa6a0fa0ff03e5cb9
Nathan Crawford <njcrawford@gmail.com>
2010-11-03 21:04:20 +0000

    Merged non-CentOS changes from centos5 branch, attempted to fix apt-get outputting noise

--- server/www/index.php
+++ server/www/index.php
@@ -3,9 +3,6 @@
 <title>Forest</title>
 </head>
 <body>
-Updates available by system
-<table>
-<tr><td>System ID</td><td>Updates Available</td><td>Reboot Required</td><td>Last Checkin</td></tr>
 <?php
 
 $updates_table = "updates";
@@ -18,82 +15,160 @@ $db_name = "forest";
 mysql_connect($db_server, $db_user, $db_password);
 mysql_select_db($db_name);
 
-$systems_result = mysql_query("select * from systems");
-
-$systems_row = mysql_fetch_assoc($systems_result);
-while($systems_row)
+?>
+<style type="text/css">
+table {
+	border-width: 1px;
+	border-spacing: 0px;
+	border-style: outset;
+	border-color: gray;
+	border-collapse: collapse;
+	background-color: white;
+}
+table th {
+	border-width: 1px;
+	padding: 1px;
+	border-style: inset;
+	border-color: gray;
+	background-color: white;
+	text-align: center;
+	padding: 5px;
+}
+table td {
+	border-width: 1px;
+	padding: 1px;
+	border-style: inset;
+	border-color: gray;
+	background-color: white;
+	text-align: center;
+	padding: 5px;
+}
+table td.name {
+	text-align: left;
+}
+form {
+	display: inline;
+}
+</style>
+<?php
+$awol_result = mysql_query("select * from systems where last_checkin < DATE_SUB(NOW(), INTERVAL 36 HOUR)");
+if(mysql_num_rows($awol_result) > 0)
 {
-	$updates_result = mysql_query("select * from updates where system_id = '" . $systems_row['id'] . "'");
-	$updates_row = mysql_fetch_assoc($updates_result);
-	
-	if($systems_row['reboot_required'] == null)
+	echo "<br /><h3>Systems that have not checked in for more than 36 hours</h3><br />";
+	echo "<table>";
+	echo "<tr><th>System name</th><th>Last checkin</th></tr>";
+	$awol_row = mysql_fetch_assoc($awol_result);
+	while($awol_row)
 	{
-		$nice_reboot = "Unknown";
+		echo "<tr><td>" . $awol_row['name'] . "</td><td>" . $awol_row['last_checkin'] . "</td></tr>";
+		$awol_row = mysql_fetch_assoc($awol_result);
 	}
-	elseif($systems_row['reboot_required'] == 1)
-	{
-		$nice_reboot = "Yes";
-	}
-	else
-	{
-		$nice_reboot = "No";
-	}
-
-	$updates_result = mysql_query("select count(package_name) as packages from updates where system_id = '" . $systems_row['id'] . "'");
-        $updates_row = mysql_fetch_assoc($updates_result);
-
-?>
-	<tr>
-		<td>
-			<a href="systems.php?name=<? echo $systems_row['name'] ?>"><? echo $systems_row['name'] ?></a>
-		</td>
-		<td><? echo $updates_row['packages'] ?></td>
-		<td><? echo $nice_reboot ?></td>
-		<td><? echo $systems_row['last_checkin'] ?></td>
-<?php
-		if($updates_row['packages'] > 0)
-		{
-?>
-		<td>
-			<form method="post" action="mark-accepted.php">
-				<input type="hidden" name="accepted" value="true">
-				<input type="hidden" name="system_id" value="<? echo $systems_row['id'] ?>">
-				<input type="submit" value="Accept all">
-			</form>
-		</td>
-<?php
-		}
-?>
-	</tr>
-<?php
-
-	$systems_row = mysql_fetch_assoc($systems_result);
+	echo "</table>";
 }
 
+$systems_final = array();
+
+$systems_result = mysql_query("select * from (select systems.id, name, count(package_name) as packages, sum(if(accepted is null, 0, accepted)) as accepted_count, reboot_required, last_checkin from systems left join (updates) on (updates.system_id = systems.id) group by systems.id) b order by packages desc");
+if(mysql_num_rows($systems_result) > 0)
+{
+	# Get systems that have updates, sorted by number of updates
+	$systems_row = mysql_fetch_assoc($systems_result);
+	while($systems_row)
+	{
+		$systems_final[$systems_row['id']] = $systems_row;
+
+		# Translate all the reboot_required values
+		if($systems_row['reboot_required'] == null)
+		{
+			$systems_final[$systems_row['id']]['reboot_required'] = "Unknown";
+		}
+		elseif($systems_row['reboot_required'] == 1)
+		{
+			$systems_final[$systems_row['id']]['reboot_required'] = "Yes";
+		}
+		else
+		{
+			$systems_final[$systems_row['id']]['reboot_required'] = "No";
+		}
+
+		$systems_row = mysql_fetch_assoc($systems_result);
+	}
+?>
+<br />
+<h3>Updates available by system</h3>
+<br />
+<table>
+<tr><th>System Name</th><th>Updates Available/Accepted</th><th>Reboot Required</th><th>Last Checkin</th></tr>
+<?php
+	foreach($systems_final as $this_system)
+	{
+?>
+	<tr>
+		<td class="name">
+			<a href="systems.php?name=<? echo $this_system['name'] ?>"><? echo $this_system['name'] ?></a>
+		</td>
+		<td><? echo $this_system['packages'] . "/" . $this_system['accepted_count'] ?></td>
+		<td><? echo $this_system['reboot_required'] ?></td>
+		<td><? echo $this_system['last_checkin'] ?></td>
+		<td>
+<?php
+		if($this_system['packages'] > 0 && $this_system['packages'] != $this_system['accepted_count'])
+		{
+?>
+			<form method="post" action="mark-accepted.php">
+				<input type="hidden" name="accepted" value="true">
+				<input type="hidden" name="system_id" value="<? echo $this_system['id'] ?>">
+				<input type="submit" value="Accept all">
+			</form>
+<?php
+		}
+		else
+		{
+			echo "&nbsp;";
+		}
+?>
+		</td>
+	</tr>
+<?php
+	}
 ?>
 </table>
-<br />
-Updates available by package name
-<table>
-<tr><td>Name</td><td>Systems</td></tr>
 <?php
-$systems_result = mysql_query("select package_name, count(system_id) as systems from updates group by package_name");
-
+}
+?>
+<br />
+<h3>Updates available by package name</h3>
+<br />
+<table>
+<tr><th>Name</th><th>Systems/Accepted</th></tr>
+<?php
+$systems_result = mysql_query("select package_name, count(system_id) as systems, sum(accepted) as accepted_count from updates group by package_name");
 $systems_row = mysql_fetch_assoc($systems_result);
 while($systems_row)
 {
 ?>
         <tr>
-		<td>
+		<td class="name">
 			<a href="packages.php?name=<? echo $systems_row['package_name'] ?>"><? echo $systems_row['package_name'] ?></a>
 		</td>
-		<td><? echo $systems_row['systems'] ?></td>
+		<td><? echo $systems_row['systems'] . "/" . $systems_row['accepted_count'] ?></td>
 		<td>
+<?php
+	if($systems_row['systems'] != $systems_row['accepted_count'])
+	{
+?>
 			<form method="post" action="mark-accepted.php">
 				<input type="hidden" name="accepted" value="true">
 				<input type="hidden" name="package" value="<? echo $systems_row['package_name'] ?>">
 				<input type="submit" value="Accept all">
 			</form>
+<?php
+	}
+	else
+	{
+		echo "&nbsp;";
+	}
+?>
 		</td>
 	</tr>
 <?php
