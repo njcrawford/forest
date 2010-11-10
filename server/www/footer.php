0754000acbb9cada9ada57084758aec97dde4bae
Nathan Crawford <njcrawford@gmail.com>
2010-11-10 03:59:16 +0000

    Added version number if available, or trunk and rev if not

--- server/www/footer.php
+++ server/www/footer.php
@@ -1,9 +1,22 @@
 <?php
+$forest_version = '$URL$';
+$forest_version = explode("/");
+$forest_version = $forest_version[count($forest_version)-4];
+
 $forest_rev = '$Revision$';
 $forest_rev = explode(" ", $forest_rev);
 $forest_rev = $forest_rev[1];
+
+if($forest_version == "trunk")
+{
+	$display_version = $forest_version . " r" . $forest_rev;
+}
+else
+{
+	$display_version = "v" . $forest_version;
+}
 ?>
 <br />
-Forest r<?php echo $forest_rev ?>
+Forest <?php echo $display_version ?>
 </body>
 </html>
