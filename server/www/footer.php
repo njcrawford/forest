c59a3c33cb00d823c1f521ab980ded388596d6f3
Nathan Crawford <njcrawford@gmail.com>
2010-11-10 03:48:29 +0000

    Corrected order of parameters

--- server/www/footer.php
+++ server/www/footer.php
@@ -1,6 +1,7 @@
 <?php
 $forest_rev = '$Revision$';
-$forest_rev = explode($forest_rev, " ")[1];
+$forest_rev = explode(" ", $forest_rev);
+$forest_rev = $forest_rev[1];
 ?>
 <br />
 Forest r<?php echo $forest_rev ?>
