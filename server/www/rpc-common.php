f09dc4e53d1310ff857074418baa4f14039777ec
Nathan Crawford <njcrawford@gmail.com>
2010-11-13 00:49:35 +0000

    Add file to handle rpc version checking

---
+++ server/www/rpc-common.php
@@ -0,0 +1,22 @@
+<?php
+$rpc_version = 1;
+$RPC_ERROR_TAG = "error: ";
+$RPC_SUCCESS_TAG = "data_ok: ";
+
+if(!empty($_GET['rpc_version']))
+{
+	$client_rpc_version = $_GET['rpc_version'];
+}
+else if(!empty($_POST['rpc_version']))
+{
+	$client_rpc_version = $_POST['rpc_version'];
+}
+else
+{
+	die($RPC_ERROR_TAG . "No rpc version specified");
+}
+if($client_rpc_version != $rpc_version)
+{
+	die($RPC_ERROR_TAG ."rpc version mismatch, server: " . $rpc_version . ", client: " . $_GET['rpc_version']);
+}
+?>
