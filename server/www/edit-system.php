bc4d86b6013513579d6e818a1b197c852a034077
Nathan Crawford <njcrawford@gmail.com>
2010-11-22 18:11:14 +0000

    Added form elements

--- server/www/edit-system.php
+++ server/www/edit-system.php
@@ -8,11 +8,15 @@ require "inc/db.php";
 $result = mysql_query("select * from systems where name = '" . $_GET['name'] . "'");
 $row = mysql_fetch_assoc($result);
 
-?>
-Name: <?php echo $row['name'] ?><br />
-Last Check-in: <?php echo $systems_row['last_checkin'] ?><br />
-Ignore AWOL: <?php echo $systems_row['ignore_awol'] ?><br />
+$nice_checked = ($row['ignore_awol'] == 1) ? "checked=checked " : "";
 
+?>
+<form>
+Name: <?php echo $row['name'] ?><br />
+Last Check-in: <?php echo $row['last_checkin'] ?><br />
+Ignore AWOL: <input name=ignore_awol type=checkbox <?php echo $nice_checked ?>></input><br />
+<input type=submit value=Save>
+</form>
 <?php
 require "footer.php";
 ?>
