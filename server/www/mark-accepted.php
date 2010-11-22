6ca0854cf6e579cd74895ca800b9de0eb48e1bdb
Nathan Crawford <njcrawford@gmail.com>
2010-11-22 18:26:13 +0000

    Updated to use db include file

--- server/www/mark-accepted.php
+++ server/www/mark-accepted.php
@@ -37,13 +37,7 @@ elseif(isset($_POST['package']))
 	$query .= "package_name = '" . $_POST['package'] . "'";
 }
 
-$db_server = "localhost";
-$db_user = "forest_user";
-$db_password = "forest_pass";
-$db_name = "forest";
-
-mysql_connect($db_server, $db_user, $db_password);
-mysql_select_db($db_name);
+require "inc/db.php";
 
 $result = mysql_query($query);
 if($result)
