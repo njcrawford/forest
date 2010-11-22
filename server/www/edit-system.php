c601302f848e27e1320858467a06f50278e65cc3
Nathan Crawford <njcrawford@gmail.com>
2010-11-22 18:49:17 +0000

    Added links to summary and system pages

--- server/www/edit-system.php
+++ server/www/edit-system.php
@@ -3,6 +3,11 @@
 $page_title = "Edit system";
 require "inc/header.php";
 
+if(!isset($_GET['name']))
+{
+	die("No system specified");
+}
+
 require "inc/db.php";
 
 $result = mysql_query("select * from systems where name = '" . $_GET['name'] . "'");
@@ -11,6 +16,8 @@ $row = mysql_fetch_assoc($result);
 $nice_checked = ($row['ignore_awol'] == 1) ? "checked=checked " : "";
 
 ?>
+<a href="./">Back to summary page</a><br />
+<a href="systems.php?name=<?php echo $_GET['name'] ?>"><?php echo $_GET['name'] ?></a><br />
 <form action="save-system.php" method="post">
 <input type="hidden" name="name" value="<?php echo $_GET['name'] ?>">
 Name: <?php echo $row['name'] ?><br />
