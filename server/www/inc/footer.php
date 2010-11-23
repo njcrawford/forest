ca7ac17b9ab649d487c93ff284dde8f2b64cd86a
Nathan Crawford <njcrawford@gmail.com>
2010-11-23 00:17:31 +0000

    Split version info into seperate file

--- server/www/inc/footer.php
+++ server/www/inc/footer.php
@@ -1,20 +1,5 @@
 <?php
-$forest_version = '$URL$';
-$forest_version = explode("/", $forest_version);
-$forest_version = $forest_version[count($forest_version)-5];
-
-/*$forest_rev = '$Revision$';
-$forest_rev = explode(" ", $forest_rev);
-$forest_rev = $forest_rev[1];
-
-if($forest_version == "trunk")
-{
-	$display_version = $forest_version . " r" . $forest_rev;
-}
-else
-{
-	$display_version = "v" . $forest_version;
-}*/
+require "version.php";
 ?>
 <br />
 <div class="footer-version">Forest <?php echo $forest_version ?></div>
---
+++ server/www/inc/version.php
@@ -0,0 +1,18 @@
+<?php
+$forest_version = '$URL: https://www.njcrawford.com/svn/forest/trunk/server/www/inc/footer.php $';
+$forest_version = explode("/", $forest_version);
+$forest_version = $forest_version[count($forest_version)-5];
+
+/*$forest_rev = '$Revision: 226 $';
+$forest_rev = explode(" ", $forest_rev);
+$forest_rev = $forest_rev[1];
+
+if($forest_version == "trunk")
+{
+	$display_version = $forest_version . " r" . $forest_rev;
+}
+else
+{
+	$display_version = "v" . $forest_version;
+}*/
+?>
