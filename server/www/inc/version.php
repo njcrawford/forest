28040a05158a091d5d86832b544065da15a06004
Nathan Crawford <njcrawford@gmail.com>
2010-11-24 17:48:56 +0000

    Moved revision and url keyword replacement to version.php

--- server/www/inc/version.php
+++ server/www/inc/version.php
@@ -1,9 +1,9 @@
 <?php
-$forest_version = '$URL: https://www.njcrawford.com/svn/forest/trunk/server/www/inc/footer.php $';
+$forest_version = '$URL$';
 $forest_version = explode("/", $forest_version);
 $forest_version = $forest_version[count($forest_version)-5];
 
-/*$forest_rev = '$Revision: 226 $';
+/*$forest_rev = '$Revision$';
 $forest_rev = explode(" ", $forest_rev);
 $forest_rev = $forest_rev[1];
 
