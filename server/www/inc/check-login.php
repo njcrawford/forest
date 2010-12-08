a94312c5c5c6ebb0774644171e8b703989ef4fd6
Nathan Crawford <njcrawford@gmail.com>
2010-12-08 23:51:05 +0000

    Added login required config option

--- server/www/inc/check-login.php
+++ server/www/inc/check-login.php
@@ -1,12 +1,16 @@
 <?php
+require_once "/etc/forest-server.conf";
 
-// check for login cookie
-if(!isset($_COOKIE['login_name']))
+// Only check for a valid login if required by config
+if($forest_config['login_required'] != false)
 {
-	require_once "/etc/forest-server.conf";
-	require_once "inc/redirect.php";
-	// no cookie, so redirect to login.php
-	redirect($forest_config['server_url'] . "/login.php");
+	// check for login cookie
+	if(!isset($_COOKIE['login_name']))
+	{
+		require_once "/etc/forest-server.conf";
+		require_once "inc/redirect.php";
+		// no cookie, so redirect to login.php
+		redirect($forest_config['server_url'] . "/login.php");
+	}
 }
-
 ?>
