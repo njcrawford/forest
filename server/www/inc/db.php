c28f55ecd6256e623575c7fde9b24c8a6d60f567
Nathan Crawford <njcrawford@gmail.com>
2011-01-01 17:55:56 +0000

    Fixed typo

--- server/www/inc/db.php
+++ server/www/inc/db.php
@@ -35,7 +35,7 @@ define("DB_VERSION", 2);
 mysql_connect($forest_config['db_server'], $forest_config['db_user'], $forest_config['db_password']) or die("Could not connect to DB");
 mysql_select_db($forest_config['db_name']) or die("Could not select DB");
 
-if(!isset($db_upgrade_mode)
+if(!isset($db_upgrade_mode))
 {
 	$version_result = mysql_query("select value from settings where name = 'db_version'");
 	$version_row = mysql_fetch_assoc($version_result);
