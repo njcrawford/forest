baf59c5cbec53ff6ee880d4dd7478031f4a135e0
Nathan Crawford <njcrawford@gmail.com>
2011-01-26 17:40:41 +0000

    Try to fix login redirect errors

--- server/www/inc/login-common.php
+++ server/www/inc/login-common.php
@@ -25,18 +25,21 @@ You can contact me at http://www.njcrawford.com/contact
 
 require_once "config-file.php";
 
-function set_logged_in_token($token)
-{
-	// this needs to be changed to a session id or something like that
-	//setcookie('login_name', $_POST['username'], 0, '/forest/');
-	session_start();
-	$_SESSION['login_name'] = $token;
-}
-
 function is_login_token_set()
 {
 	// this needs to be beefed up
 	return isset($_SESSION['login_name']);
+}
+
+function set_logged_in_token($token)
+{
+	// this needs to be changed to a session id or something like that
+	//setcookie('login_name', $_POST['username'], 0, '/forest/');
+	if(!is_login_token_set())
+	{
+		session_start();
+		$_SESSION['login_name'] = $token;
+	}
 }
 
 ?>
