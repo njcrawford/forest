59abf196f418f6b0179a97c7c3f4b3ab7f790b83
Nathan Crawford <njcrawford@gmail.com>
2011-02-01 01:22:24 +0000

    Fixed ambiguous column name

--- server/www/packages.php
+++ server/www/packages.php
@@ -37,7 +37,7 @@ require "inc/db.php";
 if(isset($_GET['name']))
 {
 	//$updates_result = mysql_query("select * from updates where package_name = '" . mysql_real_escape_string($_GET['name']) . "'");
-	$updates_result = mysql_query("select updates.system_id, updates.package_name, updates.accepted, if(update_locks.package_name is null, 0, 1) as locked from updates left outer join (update_locks) on (updates.package_name = update_locks.package_name and updates.system_id = update_locks.system_id) where package_name = '" . mysql_real_escape_string($_GET['name']) . "'");
+	$updates_result = mysql_query("select updates.system_id, updates.package_name, updates.accepted, if(update_locks.package_name is null, 0, 1) as locked from updates left outer join (update_locks) on (updates.package_name = update_locks.package_name and updates.system_id = update_locks.system_id) where updates.package_name = '" . mysql_real_escape_string($_GET['name']) . "'");
 	$updates_row = mysql_fetch_assoc($updates_result);
 
 	echo "Name: " . $updates_row['package_name'] . "<br />Systems: " . mysql_num_rows($updates_result) . "<br />";
