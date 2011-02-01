e39c9470102c6facd869639b3a0e5890ad8574f9
Nathan Crawford <njcrawford@gmail.com>
2011-02-01 21:58:42 +0000

    A test fix for systems page not showing update count

--- server/www/systems.php
+++ server/www/systems.php
@@ -46,7 +46,8 @@ if(isset($_GET['name']))
 			count(updates.package_name) as packages, 
 			sum(if(accepted is null, 0, accepted)) as accepted_count,
 			sum(if(update_locks.package_name is null, 0, 1)) as locked_count 
-		from systems left join (updates, update_locks) on (updates.system_id = systems.id and update_locks.system_id = systems.id and updates.package_name = update_locks.package_name)
+		from systems left join (updates, update_locks) on 
+			(updates.system_id = systems.id and update_locks.system_id = systems.id and updates.package_name = update_locks.package_name)
 		where name = '" . mysql_real_escape_string($_GET['name']) . "' group by systems.id"
 	);
 	$systems_row = mysql_fetch_assoc($systems_result);
@@ -65,11 +66,11 @@ if(isset($_GET['name']))
 	}
 ?>
 	Name: <?php echo $systems_row['name'] ?><br />
-	Updates: <?php echo $systems_row['packages'] ?><br />
+	Updates: <?php echo $systems_row['packages'] + $systems_row['accepted_count'] + $systems_row['locked_count'] ?><br />
 	Reboot Needed: <?php echo $nice_reboot ?><br />
 	Last Check-in: <?php echo $systems_row['last_checkin'] ?><br />
 <?php
-	if(($systems_row['packages'] - $systems_row['accepted_count'] - $systems_row['locked_count']) > 0 && ($systems_row['packages'] != $systems_row['accepted_count']))
+	if(($systems_row['packages'] - $systems_row['accepted_count'] - $systems_row['locked_count']) > 0 && (($systems_row['packages'] - $systems_row['locked_count']) != $systems_row['accepted_count']))
 	{
 ?>
 		<form method="post" action="mark-accepted-updates.php">
