8c35002db5ee2f6a534802ef2af171026d1d863c
Nathan Crawford <njcrawford@gmail.com>
2011-02-01 00:41:35 +0000

    Rolled all sql statements into one, added locked package handling

--- server/www/packages.php
+++ server/www/packages.php
@@ -36,40 +36,50 @@ require "inc/db.php";
 
 if(isset($_GET['name']))
 {
-	$updates_result = mysql_query("select * from updates where package_name = '" . mysql_real_escape_string($_GET['name']) . "'");
-	$updates_row = mysql_fetch_assoc($updates_result);
+	$updates_result = mysql_query("select *, if(update_locks.package_name is null, 0, 1) as locked 
+from updates left join (update_locks, systems) 
+on (updates.package_name = update_locks.package_name and updates.system_id = update_locks.system_id and updates.system_id = systems.id) 
+where updates.package_name = '" . mysql_real_escape_string($_GET['name']) . "'");
 
-//	$systems_result = mysql_query("select name from systems where id = '" . $row['system_id'] . "'");
-//        $systems_row = mysql_fetch_assoc($systems_result);
+	$updates_row = mysql_fetch_assoc($updates_result);
 
 	echo "Name: " . $updates_row['package_name'] . "<br />Systems: " . mysql_num_rows($updates_result) . "<br />";
 	echo "<ul>";
 	while($updates_row)
 	{
-		$systems_result = mysql_query("select name from systems where id = '" . mysql_real_escape_string($updates_row['system_id']) . "'");
-	        $systems_row = mysql_fetch_assoc($systems_result);
-		if($updates_row['accepted'] == 1)
-		{
-			$nice_accepted_value = "false";
-			$nice_button_name = "Reject";
-			$nice_checked = "checked=\"checked\"";
-		}
-		else
-		{
-			$nice_accepted_value = "true";
-			$nice_button_name = "Accept";
-			$nice_checked = "";
-		}
 ?>
 		<li>
 			<input type="checkbox" <? echo $nice_checked ?>>
-			<a href="systems.php?name=<? echo $systems_row['name'] ?>"><? echo $systems_row['name'] ?></a>
+<?php
+		if($updates_row['locked'] == 1)
+		{
+			echo " (locked)";
+		}
+		else
+		{
+			if($updates_row['accepted'] == 1)
+			{
+				$nice_accepted_value = "false";
+				$nice_button_name = "Reject";
+				$nice_checked = "checked=\"checked\"";
+			}
+			else
+			{
+				$nice_accepted_value = "true";
+				$nice_button_name = "Accept";
+				$nice_checked = "";
+			}
+?>
+			<a href="systems.php?name=<? echo $updates_row['name'] ?>"><? echo $updates_row['name'] ?></a>
 			<form method="post" action="mark-accepted-updates.php">
 				<input type="hidden" name="accepted" value="<? echo $nice_accepted_value ?>">
 				<input type="hidden" name="system_id" value="<? echo $updates_row['system_id'] ?>">
 				<input type="hidden" name="package" value="<? echo $updates_row['package_name'] ?>">
 				<input type="submit" value="<? echo $nice_button_name ?>">
 			</form>
+<?php
+		}
+?>
 		</li>
 <?php
 		$updates_row = mysql_fetch_assoc($updates_result);
