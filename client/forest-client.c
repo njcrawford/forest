997752325d6c2923f97956e803dd320371743860
Nathan Crawford <njcrawford@gmail.com>
2011-03-29 19:43:14 +0000

    Added a very basic start of C port

---
+++ client/forest-client.c
@@ -0,0 +1,49 @@
+//#############################################################################
+// Forest - a web-based multi-system update manager
+//
+// Copyright (C) 2010 Nathan Crawford
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
+//
+// A copy of the full GPL 2 license can be found in the docs directory.
+// You can contact me at http://www.njcrawford.com/contact
+//#############################################################################
+
+#define RPC_VERSION 2
+
+// define available package managers
+#define PM_APT 0
+#define PM_RPM 1
+#define PM_WINDOWS 2
+
+// for now, define what PM to use at compile time (from list above)
+#define PACKAGE_MANAGER PM_APT
+
+int main(char** args, int argc)
+{
+	char** availablePackages = NULL;
+	int availablePackageCount = 0;
+
+	// determine what packages are available to update
+
+	// check what packages have been accepted for update
+
+	// apply accepted updates (and only available updates)
+
+	// report packages that are available to update
+
+	return 0;
+}
