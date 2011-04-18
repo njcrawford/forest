ec90239e3a00974202c348eef2bf67cccd743982
Nathan Crawford <njcrawford@gmail.com>
2011-04-18 22:27:04 +0000

    Sort updates alphabetically

--- server/www/systems.php
+++ server/www/systems.php
@@ -90,7 +90,7 @@ if(isset($_GET['system_id']))
 ?>
 	<ul>
 <?php
-	$updates_result = mysql_query("select updates.package_name, updates.version, if(update_locks.package_name is null, 0, 1) as locked, updates.accepted from updates left outer join (update_locks) on (updates.system_id = update_locks.system_id and updates.package_name = update_locks.package_name) where updates.system_id = '" . $systems_row['id'] . "'");
+	$updates_result = mysql_query("select updates.package_name, updates.version, if(update_locks.package_name is null, 0, 1) as locked, updates.accepted from updates left outer join (update_locks) on (updates.system_id = update_locks.system_id and updates.package_name = update_locks.package_name) where updates.system_id = '" . $systems_row['id'] . "' order by updates.package_name");
 	for($updates_row = mysql_fetch_assoc($updates_result); $updates_row; $updates_row = mysql_fetch_assoc($updates_result))
 	{
 		if($updates_row['accepted'] == 1)
