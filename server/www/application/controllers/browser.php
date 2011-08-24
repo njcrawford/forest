2c7bb1682a63a49f34cf2e19927e83c6ceab5d59
Nathan Crawford <njcrawford@gmail.com>
2011-08-24 23:03:00 +0000

    Added proper login code, added reboot_required translating, added css classes for views to display

--- server/www/application/controllers/browser.php
+++ server/www/application/controllers/browser.php
@@ -62,8 +62,41 @@ class Browser extends CI_Controller {
 					$this_system['available_updates']++;
 				}
 			}
+			
+			// translate reboot_required value
+			$this_system['reboot_required_class'] = "";
+			if($this_system['reboot_required'] == null)
+			{
+				$this_system['reboot_required_text'] = "Unknown";
+			}
+			elseif($this_system['reboot_required'] == 1)
+			{
+				if($this_system['reboot_accepted'] == 1)
+				{
+					$this_system['reboot_required_text'] = "Accepted";
+				}
+				else
+				{
+					$this_system['reboot_required_text'] = "Yes";
+					$this_system['reboot_required_class'] = "class=\"reboot\"";
+				}
+			}
+			else
+			{
+				$this_system['reboot_required_text'] = "No";
+			}
+			
+			// use CSS class "awal" for display of check-in time of awal systems
+			if(strtotime("-3 days") > strtotime($this_system['last_checkin']))
+			{
+				$this_system['awol_class'] = "class=\"awol\"";
+			}
+			else
+			{
+				$this_system['awol_class'] = "";
+			}
 		}
-		$data['page_title'] = "Updates grouped by system name";
+		$data['page_title'] = "Summary";
 		$this->load->view('overview', $data);
 	}
 
@@ -286,7 +319,7 @@ class Browser extends CI_Controller {
 		
 		if($this->_is_logged_in())
 		{
-			return $_SESSION['login_fullname'];
+			return $_SESSION['login_name'];
 		}
 		else
 		{
@@ -302,6 +335,15 @@ class Browser extends CI_Controller {
 		$this->load->view('footer');
 	}
 	
+	private function _set_logged_in_token($token)
+	{
+		if(session_id() == "")
+		{
+			session_start();
+		}
+		$_SESSION['login_name'] = $token;
+	}
+	
 	public function log_me_in()
 	{
 		$username = $this->input->post('username');
@@ -312,57 +354,76 @@ class Browser extends CI_Controller {
 			die('Username or password not provided');
 		}
 		
-		// login stub
-		if(session_id() == "")
+		$login_success = false;
+		// do the actual login
+		if($this->config->item('login_source') == "config_file")
 		{
-			session_start();
-		}
-		$_SESSION['login_name'] = $username;
-		//$ldap_result = ldap_search($ds, $ldap_base, "(uid=" . $username . ")", array('cn'));
-		//$ldap_entries = ldap_get_entries($ds, $ldap_result);	
-		//$_SESSION['login_fullname'] = $ldap_entries[0]['cn'][0];
-		$_SESSION['login_fullname'] = $username . " (fullname)";
-		$this->load->helper('url');
-		redirect();
-		
-		// probably going to use ldap logins later, just need a stub for now
-		// bad, bad, nathan... you shouldn't hard code these things
-		/*$ldap_server = "att-srvr";
-		$ldap_auth_var = "uid";
-		$ldap_base = "ou=people,dc=atterotech,dc=com";
-		
-		$ds=ldap_connect($ldap_server);
-        if (!$ds)
-        {
-                die('Cannot connect to LDAP server.');
-        }
-        $dn = $ldap_auth_var . "=" . $username . ", " . $ldap_base;
-
-        $result=@ldap_bind($ds,$dn,$password);
-                
-		if($result)
-		{
-			if(session_id() == "")
+			foreach($this->config->item('allowed_users') as $this_user)
 			{
-				session_start();
+				if($username == $this_user['username'] && $password == $this_user['password'])
+				{
+					$login_success = true;
+					$this->_set_logged_in_token($username);
+					break;
+				}
 			}
-			$_SESSION['login_name'] = $username;
-			$ldap_result = ldap_search($ds, $ldap_base, "(uid=" . $username . ")", array('cn'));
-			$ldap_entries = ldap_get_entries($ds, $ldap_result);	
-			$_SESSION['login_fullname'] = $ldap_entries[0]['cn'][0];
-			$this->load->helper('url');
-			redirect('');
-			ldap_close($ds);
+		}
+		elseif($this->config->item('login_source') == "ldap")
+		{
+			$ds=ldap_connect($forest_config['ldap_server']);
+			if (!$ds)
+			{
+				die('Cannot connect to LDAP server.');
+			}
+			$dn = $this->config->item('ldap_auth_var') . "=" . $username . ", " . $this->config->item('ldap_base');
+		
+			$result=@ldap_bind($ds,$dn,$password);
+			
+			$ldap_allowed_users = $this->config->item('ldap_allowed_users');
+			if (!$result)
+			{
+				// Couldn't bind to LDAP server, see 
+				$lderr = ldap_error($ds);
+				if($lderr != "Invalid credentials")
+				{
+					echo "Error: " . ldap_error($ds) . "<br /><br />";
+					die('Check your user name and password and try again.');
+				}
+			}
+			elseif(!empty($ldap_allowed_users))
+			{
+				// make sure this user is allowed by config file
+				foreach($ldap_allowed_users as $this_user)
+				{
+					if($username == $this_user)
+					{
+						$login_success = true;
+						$this->_set_logged_in_token($username);
+						break;
+					}
+				}
+			}
+			else
+			{
+				$login_success = true;
+				$this->_set_logged_in_token($username);
+			}
 		}
 		else
 		{
-			$data['page_title'] = "Error";
-			$this->load->view('header', $data);
-			$data['message'] = ldap_error($ds);
-			$this->load->view('error', $data);
-			$this->load->view('footer');
+			die("Invalid login_source in config file");
 		}
-		*/
+		
+		if($login_success)
+		{
+			// redirect to summary page
+			$this->load->helper('url');
+			redirect();
+		}
+		else
+		{
+			die("Invalid login information");
+		}
 	}
 }
 ?>
