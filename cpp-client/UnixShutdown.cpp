0b6bbf1fe85299694479eb98013b43cbc913fda9
Nathan Crawford <njcrawford@gmail.com>
2011-08-26 23:04:26 +0000

    Fixed missing close quote

--- cpp-client/UnixShutdown.cpp
+++ cpp-client/UnixShutdown.cpp
@@ -4,7 +4,7 @@
 void UnixShutdown::applyReboot()
 {
 	//system("shutdown -r now");
-	system("echo \"shutdown -r now\" | at now + 10 minutes 2>&1 | grep -v \"warning: commands will be executed using\\|job [0-9]* at");
+	system("echo \"shutdown -r now\" | at now + 10 minutes 2>&1 | grep -v \"warning: commands will be executed using\\|job [0-9]* at\"");
 }
 
 bool UnixShutdown::canApplyReboot()
