90f45cda37933cb21dd8aecb0c5424c8a7536e71
Nathan Crawford <njcrawford@gmail.com>
2011-11-03 15:29:15 +0000

    Add default for email_to

--- server/www/application/config/config.php
+++ server/www/application/config/config.php
@@ -367,6 +367,8 @@ $config['db_username'] = 'forest_user';
 $config['db_password'] = 'forest_pass';
 $config['db_name'] = 'forest';
 
+$config['email_to'] = 'root';
+
 // now read in any changed values from the config file
 include "/etc/forest-server/forest-server.conf";
 
