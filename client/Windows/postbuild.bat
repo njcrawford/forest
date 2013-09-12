dcc78f56ee8ece2914348edc2cce805945862928
Nathan Crawford <njcrawford@gmail.com>
2013-09-12 11:37:17 -0400

    Update build system for new curl submodule

--- client/Windows/postbuild.bat
+++ client/Windows/postbuild.bat
@@ -1,1 +1,1 @@
-copy curl-source\builds\libcurl-vc-x86-release-dll-ipv6-sspi-spnego-winssl\bin\libcurl.dll Win32\Debug\
+copy curl-source\builds\libcurl-vc10-x86-release-dll-ipv6-sspi-spnego-winssl\bin\libcurl.dll Win32\Debug\
