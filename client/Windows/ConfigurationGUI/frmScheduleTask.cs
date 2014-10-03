aa5dca406b8c0847e166206f71edfbdf5df4e4f0
Nathan Crawford <njcrawford@gmail.com>
2014-10-03 16:11:55 -0400

    Limit Task Scheduler settings to those supported on the running OS

--- client/Windows/ConfigurationGUI/frmScheduleTask.cs
+++ client/Windows/ConfigurationGUI/frmScheduleTask.cs
@@ -28,6 +28,9 @@ namespace ConfigurationGUI
             // Get the service on the local machine
             using (TaskService ts = new TaskService())
             {
+                // FIgure out what version of task scheduler we're using
+                bool isVersionOne = ts.HighestSupportedVersion < new Version(1, 2);
+
                 // Create a new task definition and assign properties
                 TaskDefinition td = ts.NewTask();
                 td.RegistrationInfo.Description = "Runs Forest Client daily";
@@ -42,18 +45,27 @@ namespace ConfigurationGUI
                 // Change some settings of the task to fit our application.
                 // It may make sense to break some of these settings out as controls the user can adjust.
                 td.Settings.RunOnlyIfNetworkAvailable = true;
-                // Stop and existing instances when starting a new one. This
-                // shouldn't ever happen, as far as I can tell.
-                td.Settings.MultipleInstances = TaskInstancesPolicy.StopExisting;
+
                 // Don't stop just because someone started using the system
                 td.Settings.IdleSettings.StopOnIdleEnd = false;
+
                 // Run even if system is not idle
                 td.Settings.RunOnlyIfIdle = false;
-                // If the scheduled time is missed, start as soon as the system is available
-                td.Settings.StartWhenAvailable = true;
+
                 // Allow the app to run for up to 6 hours. This should be enough
                 // to install updates, if/when we're ever able to do so on Windows.
-                td.Settings.ExecutionTimeLimit = new TimeSpan(6, 0, 0);
+                td.Settings.ExecutionTimeLimit = TimeSpan.FromHours(6);
+
+                // Some things arent compatible with older versions of task scheduler
+                if (!isVersionOne)
+                {
+                    // Stop and existing instances when starting a new one. This
+                    // shouldn't ever happen, as far as I can tell.
+                    td.Settings.MultipleInstances = TaskInstancesPolicy.StopExisting;
+
+                    // If the scheduled time is missed, start as soon as the system is available
+                    td.Settings.StartWhenAvailable = true;
+                }
 
                 // Register the task in the root folder
                 ts.RootFolder.RegisterTaskDefinition(@"Forest Client", td,
@@ -73,7 +85,7 @@ namespace ConfigurationGUI
         {
             // Limit the date range to today
             dateTimePicker1.MinDate = DateTime.Today;
-            dateTimePicker1.MaxDate = DateTime.Today + new TimeSpan(1, 0, 0, 0);
+            dateTimePicker1.MaxDate = DateTime.Today + TimeSpan.FromDays(1);
 
             // Default to 3:05 AM
             DateTime defaultRunTime = DateTime.Today + new TimeSpan(3, 5, 0);
