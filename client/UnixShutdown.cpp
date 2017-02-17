5de5b60407544141ddcec24bcdf33efe7db928f9
Nathan Crawford <njcrawford@gmail.com>
2017-02-16 20:12:00 -0500

    Check for 'at' command when deciding if this PC can be rebooted (resolves #29), and improve some comments around configure and config.h

--- client/UnixShutdown.cpp
+++ client/UnixShutdown.cpp
@@ -5,10 +5,18 @@ void UnixShutdown::applyReboot()
 {
 	// Initiate a shutdown in 10 minutes
 	// The 10 minute delay is to allow other cron jobs to finish
-	system("echo \"/sbin/shutdown -r now\" | at now + 10 minutes 2>&1 | grep -v \"warning: commands will be executed using\\|job [0-9]* at\"");
+	system("echo \"/sbin/shutdown -r now\" | " AT_COMMAND " now + 10 minutes 2>&1 | grep -v \"warning: commands will be executed using\\|job [0-9]* at\"");
 }
 
 bool UnixShutdown::canApplyReboot()
 {
-	return true;
+	// Check for 'at' presence
+	if (access(AT_COMMAND, F_OK) != -1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
